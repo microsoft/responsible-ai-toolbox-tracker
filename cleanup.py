4f04212c5461902d2551e892a457bc3f8882cca2
Dany Rouhana <danyr@microsoft.com>
2023-05-03 08:58:24 -0700

    npm release.  version issues.

--- cleanup.py
+++ cleanup.py
@@ -3,9 +3,11 @@ import shutil
 
 directory = 'dist'
 keep_directory = 'raitracker'
+keep_file = '.tgz'
 
 for filename in os.listdir(directory):
-    if not filename.startswith(keep_directory):
+    # if not filename.startswith(keep_directory):
+    if not filename.endswith(keep_file):
         file_path = os.path.join(directory, filename)
         if os.path.isfile(file_path) or os.path.islink(file_path):
             os.unlink(file_path)
